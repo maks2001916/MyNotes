b06049ba6de2d12d4d30eb7d862ae437761eb32e
maks2001916 <mamonov.maksim.20003@gmail.com>
2025-02-02 14:33:22 +0500

    Элементы вёрстки присвоены соответствующим объектам

--- app/src/main/java/com/example/mynotes/FirstFragment.kt
+++ app/src/main/java/com/example/mynotes/FirstFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.RecyclerView
 
 class FirstFragment : Fragment() {
 
@@ -14,4 +18,15 @@ class FirstFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var toolbarTB = view.findViewById(R.id.toolbarTB) as Toolbar
+        var enterTextET = view.findViewById(R.id.enterTextOfNoteET) as EditText
+        var saveBTN = view.findViewById(R.id.saveBTN) as Button
+        var listRV = view.findViewById(R.id.listRV) as RecyclerView
+
+    }
+
 }
